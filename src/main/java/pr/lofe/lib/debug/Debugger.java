3bd115da08f6a25d7a2e852ba71255dc247ab33a
just_lofe <97516390+justlofe@users.noreply.github.com>
2024-05-16 20:09:56 +0700

    Create commit

---
+++ src/main/java/pr/lofe/lib/debug/Debugger.java
@@ -0,0 +1,17 @@
+package pr.lofe.lib.debug;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Debugger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
